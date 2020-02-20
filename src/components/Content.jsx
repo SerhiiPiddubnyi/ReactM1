98ec45e38728bbf4e175212276c4fa9ce0fcd6fc
Serhii Piddubnyi <sergeu_ne@ukr.net>
2020-02-20 15:48:39 +0200

    'Components'

---
+++ src/components/Content.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Content = () =>{
+    return(
+        <div className='content'>
+            <div>
+                <img src='https://www.w3schools.com/howto/img_snow_wide.jpg'/>
+            </div>
+            <div>
+                ava + description
+            </div>
+            <div>
+                My posts
+                <div>
+                    New post
+                </div>
+                <div>
+                    Post1
+                </div>
+                <div>
+                    post2
+                </div>
+            </div>
+            <div></div>
+        </div>
+    );
+}
+
+export  default Content;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Navbar = () =>{
+    return(
+        <nav className='nav'>
+            <div>
+                <a>Main menu</a>
+            </div>
+            <div>
+                <a>Messages</a>
+            </div>
+            <div>
+                <a>News</a>
+            </div>
+            <div>
+                <a>Videos</a>
+            </div>
+            <div>
+                <a>Setting</a>
+            </div>
+        </nav>
+    );
+}
+
+export  default Navbar;
